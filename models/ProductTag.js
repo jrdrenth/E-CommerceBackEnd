8170fc796dd3a1d3b9000cb04c6d9e484d3dfdb2
jrdrenth <john.drenth@gmail.com>
2021-03-15 00:01:48 -0700

    created unique composite key in junction table (ProductTag)

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -15,18 +15,14 @@ ProductTag.init(
     product_id: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      references: {
-        model: 'product',
-        key: 'id',
-      }
+      unique: 'uk_productId_tagId', // composite unique key by using same 'unique:' string for multiple columns
+      references: { model: 'product', key: 'id' }
     },
     tag_id: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      references: {
-        model: 'tag',
-        key: 'id',
-      }
+      unique: 'uk_productId_tagId', // composite unique key by using same 'unique:' string for multiple columns
+      references: { model: 'tag', key: 'id' }
     }
   },
   {
